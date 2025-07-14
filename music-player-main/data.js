77d9aef0779f443a098661d2e0f07662bf832a2a
Hale Akbas <146046440+haleakbas@users.noreply.github.com>
2025-07-14 18:58:03 -0400

    Add files via upload

---
+++ music-player-main/data.js
@@ -0,0 +1,30 @@
+let songs = [
+    {
+        name: 'fall',
+        path: 'Jurrivh_Fall.mp3',
+        artist: 'Joni Van Rossum, Jurrivh',
+        cover: 'fall.jpg'
+    },
+    
+    {
+        name: '忘れじの言の葉',
+        path: 'dazbee.mp3',
+        artist:'DAZBEE',
+        cover: 'dazbeecover.jpg'
+    
+    },
+    {
+        name:'primrose, the dancer',
+        path: 'primrose.mp3',
+        artist: 'Patti Rudisill',
+        cover: 'primcover.jpg'
+    },
+
+    {
+        name: '英雄の詩篇',
+        path: 'mirai.mp3',
+        artist: 'Mirai Kodai Gakudan',
+        cover: 'miraicover.png'
+    },
+    
+    ]
